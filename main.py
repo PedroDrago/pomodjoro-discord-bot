f874b9eb7b8632216f1930cde2b448bb930a6184
Drago <pedrodrago.profissional@gmail.com>
2022-09-27 08:59:47 -0300

    fix: messages were not being awaited

--- main.py
+++ main.py
@@ -16,17 +16,16 @@ async def on_ready():
 @client.command()
 async def djstart(ctx, pomodoro=25, short_break=5, long_break=10, cycles=4):
     channel = client.get_channel(1024003232227393576)
-    channel.send('Pomodoro initialized with times bla bla bla')
-    while True:
-        for c in range(cycles):
-            channel.send(f'Starting cycle {c+1}')
-            sleep(pomodoro) # *60 after testing
-            channel.send(f'End of study time')
-            sleep(short_break)
-            channel.send(f'End of short break.')
-        channel.send(f'Cycles have endend.')
-        sleep(long_break)
-        channel.send('End of long break.')
+    await channel.send('Pomodoro initialized with times bla bla bla')
+    for c in range(cycles):
+        await channel.send(f'Starting cycle {c+1}')
+        sleep(pomodoro * 60) # *60 after testing
+        await channel.send(f'End of study time')
+        sleep(short_break * 60)
+        await channel.send(f'End of short break.')
+    await channel.send(f'Cycles have endend.')
+    sleep(long_break * 60)
+    await channel.send('End of long break.')
             
 
 
