626e6620bbfae08fd6d59eb5df0bfe8be4a28683
Drago <pedrodrago.profissional@gmail.com>
2022-09-27 08:08:27 -0300

    feat: connect bot to discord via token in a .env

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
-client = commands.bot(command_prefix = '!')
+client = commands.Bot(intents=discord.Intents.all() ,command_prefix = '!')
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
 
 @client.event
 async def on_ready():
     print('Bot have been successfully initialized.')
     print('_______________________________________')
+
+client.run(TOKEN)
