2cea7fbf6d3cb7b372b5accd77b738137ea63027
Drago <pedrodrago.profissional@gmail.com>
2022-09-27 07:56:26 -0300

    feat: prompt on ready message when bot is initialized

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from discord.ext import commands
 
 client = commands.bot(command_prefix = '!')
 
+@client.event
+async def on_ready():
+    print('Bot have been successfully initialized.')
+    print('_______________________________________')
